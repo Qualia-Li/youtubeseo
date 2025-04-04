00da75522e0ba98bbc2bdf05532a66c7997fcf3b
Qualia-Li <Qualia-Li@users.noreply.github.com>
2025-04-04 13:11:40 -0700

    feat: Add Google Analytics component for tracking

---
+++ src/components/GoogleAnalytics.tsx
@@ -0,0 +1,45 @@
+'use client';
+
+import Script from 'next/script';
+import { usePathname, useSearchParams } from 'next/navigation';
+import { useEffect } from 'react';
+
+// Google Analytics Measurement ID
+const GA_MEASUREMENT_ID = 'G-J4XLM9GESD';
+
+export default function GoogleAnalytics() {
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+
+  useEffect(() => {
+    if (pathname && window.gtag) {
+      // Send pageview with updated path
+      window.gtag('config', GA_MEASUREMENT_ID, {
+        page_path: pathname,
+      });
+    }
+  }, [pathname, searchParams]);
+
+  return (
+    <>
+      <Script 
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
+      />
+      <Script
+        id="google-analytics"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GA_MEASUREMENT_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
+    </>
+  );
+} 
